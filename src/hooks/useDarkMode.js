9a02da8bc8c2669966c740335ffb40b49592abdb
Abhishek EH <collegewap@gmail.com>
2020-08-29 13:02:33 +0530

    useDarkMode hook

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+
+export default () => {
+  const [isDark, setIsDark] = useState(false);
+
+  useEffect(() => {
+    const className = "dark";
+    if (isDark) {
+      window.document.body.classList.add(className);
+    } else {
+      window.document.body.classList.remove(className);
+    }
+  }, [isDark]);
+
+  return [isDark, setIsDark];
+};
